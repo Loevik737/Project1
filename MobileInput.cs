54c74d8d8517c4305d0dee2030386d28465e5721
Håvard Aspen Løvik <hloevi@live.no>
2017-01-22 15:04:31 +0100

    added mobileinput support

---
+++ MobileInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileInput : MonoBehaviour {
+
+	public GameObject player;
+	private PlayerAutoScroll playerScrpt;
+
+	void Start(){
+		playerScrpt = player.GetComponent<PlayerAutoScroll> ();
+	}
+	void Update(){
+
+		if (Input.touches.Length > 0) {
+			playerScrpt.Jump(true);
+		}
+	}
+}
